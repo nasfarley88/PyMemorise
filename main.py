89e4fe8698c74e5e4dad284564d40795d8ae434c
Nathanael Farley <nasfarley88@gmail.com>
2012-02-12 22:35:49 +0000

    Trying to match strings, not working with [\(\)] yet

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 #!/usr/bin/python3.2
+import re
 
-print('hello world')
+"""Very simply compare the given string to a second given string."""
+
+#test_string = input("Please input a test string to compare: ")
+#file = open('temp.pym', 'w')
+#file.write(test_string)
+#file.close()
+file = open('temp.pym', 'r')
+
+file_string = file.read()
+print(file_string)
+# Matches every word and leading/trailing punctuation
+#test_string = re.findall('[\w!?",\'.\(\)]+', file_string)
+
+# Matches every word and every relavant punctuation mark
+test_string = re.findall('\w+|[!?",\'.\(\)]', file_string)
+
+print(test_string)
+compare_string = input("Please input string for comparison: ")
+compare_string = re.findall('\w+|[!?",\'.\(\)]', compare_string)
+print(compare_string)
+for i in range(0,len(test_string)):
+	if re.match(re.compile(compare_string[i]), test_string[i]):
+		print('1')
+else:
+	print('0')
+
+
+#print(x)
+file.close()
+	
