3badb998e3e7c23396cce304a579b888372911e1
Nathanael <nasfarley88@alice-ubuntu.(none)>
2012-05-27 15:13:59 +0100

    Reorganised code for readability

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from termcolor import colored, cprint
 #TODO: known bugs
 # - allowed punctuation does not spark wrong word at the beggining of the word.
 #   i.e. 'word will match word but not w'ord will not match word.
+# 
+# This program needs a big debug. A proper, awake, work through to test the logic
+# It's too twisted to change now.
 
 """Very simply compare the given string to a second given string.
 
@@ -60,70 +63,79 @@ def check_string(test_string,compare_string,test_string_punc):
 
 		cprint(test_string_punc[i], 'yellow', end=' ')
 
-
-def check_string_2(test_string,compare_string,test_string_punc):
+# Redesign this function with the string splitting happening inside.
+def check_string_2(test_string,compare_string):
 	"""Check if the string is correct -- BibleMemorizer technique."""
+
+	# Split the strings for processing
+	test_string = re.findall('\w+|[;:,.]', test_string)
+	compare_string = re.findall('\w+|[,.;:]', compare_string)
+	
+	# Some debug output
+	print('The test_string is: ', test_string)
+	print('The compare_string is:', compare_string)
+
 	# I'm thinking, we make this function, the one that checks the method we need to use.
 	min_length = min(len(test_string),len(compare_string))
+
+	# Various variable to change which strings are compared.
 	i_user_offset = 0
 	wrong_counter = 0
 	i_real_offset = 0
+
+	# This loop goes across the string lists until one runs out.
 	for i_real in range(0,min_length):
+
+		# match_is_true allows the loop to know if a match has happened.
 		match_is_true = False
+
+		# TODO I'm not sure what this is...
 		i = i_real + i_real_offset
-		print()
-		print('i loop entered, where i =', i, 'i_user_offset =', i_user_offset, "i_real_offset =", i_real_offset)
-		# Compare the compare_string to see if it matches the test_string and vice versa
-		print('I want to compare', test_string[i], 'and', compare_string[i+i_user_offset])
+		
+		# if the current string matches... something? I'm not clear on what these i's do.
 		if re.match(re.compile(test_string[i]), compare_string[i+i_user_offset]) \
 				and re.match(re.compile(compare_string[i+i_user_offset]), test_string[i]):
-			print()
-			print("MATCH")
-			# Print any missing words
+
+			# If there are wrong words from something before, print them before printing the match.
 			if wrong_counter > 0:
 				for j in range (i-wrong_counter,i):
 					cprint(test_string[j], 'blue', end='')
 				
 				# Reset the counter, it's done it's job
 				wrong_counter = 0
-			#if matches, print the test string equivalent (saying it's good.)
+			# Print the match.
 			cprint(test_string[i], 'yellow', end='')
 		else:
+			# If it doesn't match, then loop over the rest of the compare_string 
 			for j in range(i,min_length):
-				#print()
-				print('j loop entered, j =', j, ' i =', i)
 				if re.match(re.compile(test_string[j]), compare_string[i+i_user_offset]) \
 						and re.match(re.compile(compare_string[i+i_user_offset]), test_string[j]):
+					# If it matches, print the missing words (in blue), reset the wroung_counter and change... variables?!
 					for k in range(i,j):
-						#print()
-						print('k loop entered, k =', k, ' j =', j, ' i =', i)
 						# Print the words in between, and the one's counted by wrong_counter
 						cprint(test_string[k], 'blue', end='')
 						# Because it's output them now.
 						wrong_counter = 0
-					#i_user_offset = i_user_offset + i_real + i_real_offset - j
+					# TODO ISSUES HERE. What are these variables, and how do they work so well?
 					i_user_offset = i_user_offset + i - j
-					print('i_user_offset =', i_user_offset)
-					#i_real_offset = j - i_real
 					i_real_offset = i_real_offset + j - i
-					print('i_real_offset =', i_real_offset)
-					#print('Before the change j =', j, 'and i =', i)
 					# Act as if it is the right one.
 					cprint(test_string[i_real+i_real_offset], 'yellow', end='')
-					#print()
-					print('Changed j to', j, 'i to', i, 'i_user_offset to', i_user_offset)
 					match_is_true = True
 					break # break the loop, we've found what we're looking for.
-
+				
+			# If the previous for loop didn't find anything then print the word as wrong... and again TODO VARIABLES?!
 			if match_is_true == False:
 				cprint(compare_string[i+i_user_offset], 'red', end='')
-				print('I couldn\'t find the word you\'re looking for...')
+
+				# TODO These variables, what are they here for?! Same one 
 				i_real_offset = i_real_offset - 1
 				i_user_offset = i_user_offset + 1
 				# Can't find a word? Count how many here.
 				#wrong_counter = wrong_counter + 1
 
-
+		# This if is supposed to print the rest of the string in the event it's not already printed by the end of the testing.
+		# In order to fix this, I *need* to understand how my variables work. I'll draw a diagram tomorrow. (Sunday 27th May)
 		if i + i_user_offset > min_length or i + i_real_offset > min_length:
 			print("OH MY GOSH IT'S ALL GOING TO--")
 			print('len(test_string) =', len(test_string), "i+i_user_offset =", i+i_user_offset)
@@ -134,9 +146,12 @@ def check_string_2(test_string,compare_string,test_string_punc):
 			if i + i_real_offset > len(compare_string):
 				for j in range(i + i_real_offset+1, len(compare_string)):
 					cprint(compare_string[j], 'red', end=' ')
+			# TODO I'm unclear as to why this break is here. But it does 'fix' stuff.
+			# Maybe rewrite the if loop, with a proper understanding of the variables.
 			break # break, because the user string has come to an end before i loop is ready to give up.
 
 def no_of_punc(x):
+	""" I may not need this function anymore... using different methods for punctuation."""
 	#TODO possible error when there is a newline in the quote (i.e. a '\n')
 	y = "no_of_punc didn't work."
 	#y = re.findall('([\w\s\'-,]+)[\W\s\'-]+', x)
@@ -166,19 +181,11 @@ file = open('temp_test.pym', 'r')
 
 #file_string = file.read()
 test_string = file.read()
-test_string_unchanging = test_string # remove this variable later, restructure the programe to have only one unchangeable variable
+test_string_unchanging = test_string # TODO remove this variable later, restructure the programe to have only one unchangeable variable
 test_string_punc = test_string # this will be formatted later
 #print(file_string)
 print(test_string)
 
-# New plan: catalog phrases, not words. Let's match phrases
-#test_string = re.findall('([\w\s\'-,]+)[\W\s\'-]+', test_string)
-test_string = re.findall('([\w\'-,]+)[\W\'-]+', test_string)
-print(test_string)
-
-# I need to extract the punctuation
-test_string_punc = re.findall('[,.;:]', test_string_punc)
-print(test_string_punc)
 
 print('\n\n###Time for comparison###\n\n')
 
@@ -186,25 +193,5 @@ print('\n\n###Time for comparison###\n\n')
 file_compare = open('temp_compare.pym', 'r')
 compare_string = file_compare.read()
 
-#compare_string = re.findall('([\w\s\'-,]+)[\W\s\'-]+', compare_string)
-compare_string = re.findall('([\w\'-,]+)[\W\'-]+', compare_string)
-print(compare_string)
-print()
-
-
-
-################### everyting after this needs to be functioned out. ###############
-
 # Time to show the user what's happened to their string
-#test_string_watchman = [0] * len(test_string)
-#compare_string_watchman = [0] * len(compare_string)
-# TODO this will be reinstaed when I have done the BibleMemoriser technique
-#check_string(test_string,compare_string,test_string_punc)
-check_string_2(test_string,compare_string,test_string_punc)
-
-no_of_punc(test_string_unchanging)
-
-
-
-# just a print to get the next line.
-print()
+check_string_2(test_string,compare_string)
